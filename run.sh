de81db969b8657f4f14b6c4ff1639b7f16c312bf
Platon Efimov <platon.efimov@gmail.com>
2022-03-21 07:54:12 +0700

    Force Google DNS

--- run.sh
+++ run.sh
@@ -8,6 +8,8 @@ docker run -d \
   --restart always \
   --cap-add=NET_ADMIN \
   -p 21783:3128 \
+  --dns 8.8.8.8 \
+  --dns 8.8.4.4 \
   --env-file=.env \
   -v /opt/squid/cache:/var/spool/squid \
   -v /opt/squid/squid.conf:/etc/squid/squid.conf \
